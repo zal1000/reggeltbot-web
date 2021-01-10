33c46574cd718e689c19e6deb9080f7bf3f78b75
Zalán Horváth <pityuespetra@gmail.com>
2021-01-10 04:35:31 +0100

    Update count.component.ts

--- src/app/count/count.component.ts
+++ src/app/count/count.component.ts
@@ -1,7 +1,10 @@
 import { stringify } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-count',
@@ -12,7 +15,17 @@ export class CountComponent implements OnInit {
   reggeltcount = "Loading..." 
   noid = false;
   dcpp = null;
-  constructor(private db: AngularFirestore, private route: ActivatedRoute) { }
+  tag:string = "zal1000#9238";
+  dcids = [
+    {
+        "id": "423925286350880779",
+        "tag": "zal1000#9238",
+        "pp": "https://cdn.discordapp.com/avatars/423925286350880779/a_2359f5d0b19f0441cad31c91b45b1249.webp",
+        "reggeltcount": 159,
+        "username": "zal1000"
+    }
+];
+  constructor(private db: AngularFirestore, private route: ActivatedRoute, private http: HttpClient, private router: Router,) { }
   
 
   ngOnInit(): void {
@@ -33,6 +46,24 @@ export class CountComponent implements OnInit {
           this.dcpp = docasd.data().pp
         }
       })
+  } 
   }
+
+  getUser() {
+    this.getUserByTag(this.tag).subscribe(
+      (response) => {
+        this.dcids = response;
+      })
   }
+
+  getUserByTag(username: string): Observable<any> {
+    return this.http.post('http://localhost:8081/reggeltbot/users', {
+      username: username,
+    });
+  }
+// https://api.zal1000.net
+
+// http://localhost:8081
+
+// https://latest---api-zd72hz742a-uc.a.run.app
 }
