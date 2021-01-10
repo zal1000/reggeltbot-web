bad75c61521f5799e51ddbbc14275ef559f6adcd
Zalán Horváth <pityuespetra@gmail.com>
2021-01-10 14:11:49 +0100

    asd

--- src/app/count/count.component.ts
+++ src/app/count/count.component.ts
@@ -1,7 +1,7 @@
 import { stringify } from '@angular/compiler/src/util';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
@@ -11,20 +11,20 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   templateUrl: './count.component.html',
   styleUrls: ['./count.component.css']
 })
-export class CountComponent implements OnInit {
+export class CountComponent implements OnInit, OnDestroy {
   reggeltcount = "Loading..." 
   noid = false;
   dcpp = null;
-  tag:string = "zal1000#9238";
+  tag = "";
   dcids = [
     {
-        "id": "423925286350880779",
-        "tag": "zal1000#9238",
+        "id": "noid",
+        "tag": "Loading...",
         "pp": "https://cdn.discordapp.com/avatars/423925286350880779/a_2359f5d0b19f0441cad31c91b45b1249.webp",
-        "reggeltcount": 159,
-        "username": "zal1000"
+        "username": "Loading..."
     }
 ];
+  mySubscription: any;
   constructor(private db: AngularFirestore, private route: ActivatedRoute, private http: HttpClient, private router: Router,) { }
   
 
@@ -53,17 +53,40 @@ export class CountComponent implements OnInit {
     this.getUserByTag(this.tag).subscribe(
       (response) => {
         this.dcids = response;
+        console.log(response)
+        console.log(this.tag)
       })
+      console.log(this.tag)
   }
 
   getUserByTag(username: string): Observable<any> {
-    return this.http.post('http://localhost:8081/reggeltbot/users', {
+    return this.http.post('https://latest---api-zd72hz742a-uc.a.run.app/reggeltbot/users', {
       username: username,
     });
+
   }
 // https://api.zal1000.net
 
 // http://localhost:8081
 
 // https://latest---api-zd72hz742a-uc.a.run.app
+
+ngOnDestroy() {
+  if (this.mySubscription) {
+    this.mySubscription.unsubscribe();
+  }
+}
+
+  reload() {
+    this.router.routeReuseStrategy.shouldReuseRoute = function () {
+      return false;
+    };
+    
+    this.mySubscription = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Trick the Router into believing it's last link wasn't previously loaded
+        this.router.navigated = false;
+      }
+    });
+  }
 }
--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, combineChange } from '@angular/fire/firestore';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-leaderboard',
@@ -11,8 +13,9 @@ import { Observable } from 'rxjs';
 export class LeaderboardComponent implements OnInit {
 
   users = null;
+  mySubscription: any;
 
-  constructor(private db: AngularFirestore, private http: HttpClient) { }
+  constructor(private db: AngularFirestore, private http: HttpClient, private router: Router,) { }
 
   ngOnInit(): void {
     this.getList().subscribe(
@@ -30,5 +33,24 @@ export class LeaderboardComponent implements OnInit {
   getList(): Observable<any> {
     return this.http.get('https://api.zal1000.net/reggeltbot/leaderboard');
   }
+
+  ngOnDestroy() {
+    if (this.mySubscription) {
+      this.mySubscription.unsubscribe();
+    }
+  }
+  
+    reload() {
+      this.router.routeReuseStrategy.shouldReuseRoute = function () {
+        return false;
+      };
+      
+      this.mySubscription = this.router.events.subscribe((event) => {
+        if (event instanceof NavigationEnd) {
+          // Trick the Router into believing it's last link wasn't previously loaded
+          this.router.navigated = false;
+        }
+      });
+    }
 
 }
