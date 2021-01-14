0c5373aa5a8ede2047aa5b7d46728d9ed168f694
Zalán Horváth <pityuespetra@gmail.com>
2021-01-14 16:08:24 +0100

    asd

--- src/app/countsearch/countsearch.component.ts
+++ src/app/countsearch/countsearch.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-countsearch',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./countsearch.component.scss']
 })
 export class CountsearchComponent implements OnInit {
-
-  constructor() { }
+  name: string = "zal100";
+  buttons = [
+    {
+      tag: "Loading...",
+      pp: environment.loadgif
+    }
+  ]
+  constructor(private db: AngularFirestore) { }
 
   ngOnInit(): void {
+    this.getUsers()
   }
 
+  getUsers() {
+    const btns: { tag: any; pp: any; }[] = [];
+    const ref = this.db.firestore.collection('dcusers').where('username', '==', this.name);
+    ref.get().then((docs: any) => {
+        docs.forEach((doc: any) => {
+          btns.push({
+            tag: doc.data().tag,
+            pp: doc.data().pp,
+          })
+        });
+    }).then(() => {
+      if(btns.length === 0) {
+        btns.push({
+          tag: "Error: user not found",
+          pp: environment.error
+        })
+        this.buttons = btns;
+        console.log(btns)
+      } else {
+        this.buttons = btns;
+        console.log(btns)
+      }
+
+    })
+  }
 }
