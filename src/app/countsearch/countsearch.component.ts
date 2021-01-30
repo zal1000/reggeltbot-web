1a988529aaf02e1237306bfcb83df78db991e460
Orosfai I. Zsolt <orosfai11@gmail.com>
2021-01-30 16:40:49 +0100

    Add offline error msg

--- src/app/countsearch/countsearch.component.ts
+++ src/app/countsearch/countsearch.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input  } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -12,7 +12,7 @@ import { CountComponent } from '../count/count.component';
 })
 export class CountsearchComponent implements OnInit {
   form!: FormGroup;
-  buttons =[
+  buttons = [
     {
       tag: "Please search for user",
       pp: null,
@@ -26,11 +26,14 @@ export class CountsearchComponent implements OnInit {
   dcid!: string;
   loading = false;
   pure = true;
+  online: boolean = false;
 
   constructor(private db: AngularFirestore, private fb: FormBuilder) { }
-  
+
   ngOnInit() {
-    if(location.search.slice(1).split("&")[0].split("=")[1]){
+    this.online = window.navigator.onLine;
+
+    if (location.search.slice(1).split("&")[0].split("=")[1]) {
       this.empty = false;
       this.pure = false;
       this.buttons.push({
@@ -45,18 +48,21 @@ export class CountsearchComponent implements OnInit {
     console.log(location.search.slice(1).split("&")[0].split("=")[1])
     this.form = new FormGroup({
       name: new FormControl()
-   })
+    })
   }
 
   getUsers(f: NgForm) {
-    this.loading = true;
-    this.pure = false;
-    console.log(f.value)
-    const name = f.value.name;
-    console.log(name)
-    const btns: { tag: any; pp: any; id: any; err: boolean; loading: boolean; }[] = [];
-    const ref = this.db.firestore.collection('dcusers').where('username', '==', name);
-    ref.get().then((docs: any) => {
+    this.online = window.navigator.onLine;
+
+    if (this.online) {
+      this.loading = true;
+      this.pure = false;
+      console.log(f.value)
+      const name = f.value.name;
+      console.log(name)
+      const btns: { tag: any; pp: any; id: any; err: boolean; loading: boolean; }[] = [];
+      const ref = this.db.firestore.collection('dcusers').where('username', '==', name);
+      ref.get().then((docs: any) => {
         docs.forEach((doc: any) => {
           btns.push({
             tag: doc.data().tag,
@@ -66,26 +72,26 @@ export class CountsearchComponent implements OnInit {
             loading: false,
           })
         });
-    }).then(() => {
-      if(btns.length === 0) {
-        btns.push({
-          tag: "Error: user not found",
-          pp: environment.error,
-          id: null,
-          err: true,
-          loading: false,
-        })
-        this.buttons = btns;
-        console.log(btns)
-      } else {
-        this.buttons = btns;
-        console.log(btns)
-      }
-      this.loading = false;
-    }).catch(err => {
-      console.log(err)
-      this.loading = false;
-    })
+      }).then(() => {
+        if (btns.length === 0) {
+          btns.push({
+            tag: "Error: user not found",
+            pp: environment.error,
+            id: null,
+            err: true,
+            loading: false,
+          })
+          this.buttons = btns;
+          console.log(btns)
+        } else {
+          this.buttons = btns;
+          console.log(btns)
+        }
+        this.loading = false;
+      }).catch(err => {
+        console.log(err)
+        this.loading = false;
+      })
+    }
   }
-  
 }
