2304d1c3ac07ebfc33f83ddaaedb35f4e7f596c8
Zalán Horváth <pityuespetra@gmail.com>
2021-02-05 01:26:10 +0100

    asd

---
+++ src/app/console/premium/premium.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth.service';
+import { environment } from 'src/environments/environment';
+
+declare var StripeCheckout: StripeCheckoutStatic;
+
+@Component({
+  selector: 'app-premium',
+  templateUrl: './premium.component.html',
+  styleUrls: ['./premium.component.scss']
+})
+export class PremiumComponent implements OnInit {
+
+  constructor(private auth: AuthService) { }
+
+  handler!: StripeCheckoutHandler;
+
+  confirmation: any;
+  loading = false;
+  apiurl = environment.apiurl;
+
+  ngOnInit(): void {
+    this.handler = StripeCheckout.configure({
+      key: 'pk_test_your_key',
+      image: '/your-avatar.png',
+      locale: 'auto',
+      source: async (source) => {
+        this.loading = true;
+        const user: any = await this.auth.getUser();
+        await fetch(`${this.apiurl}/stripe/reggeltbot/premium?u=${user.uid}`, {
+          method: 'POST',
+        }).then(res => {
+          this.confirmation = res.json();
+        }).catch(err => {
+          this.confirmation = err;
+        })
+
+        this.loading = false;
+
+      }
+    });
+  }
+
+}
